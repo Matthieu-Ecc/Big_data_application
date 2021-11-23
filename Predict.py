ad5f823ada0a1b223e520884c4bc18388361a832
matthieu <matthieu.eccher@free.fr>
2021-11-23 08:40:26 +0100

    Shap explanation model

--- Predict.py
+++ Predict.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from os import path
 import numpy
 import pandas as pd
 
-X = pd.read_csv("X_test_prod_ready.csv")
+X = pd.read_csv("data/X_test_prod_ready.csv")
 
 X = X.drop(columns='Unnamed: 0')
 
-
 from joblib import load
-
-xgboost = load('model/Model_XGboost')
-
-tree_classifier = load('model/Model_tree_classifier')
-
-Gradient_boosting_classifiers = load('model/gbc')
-
 
 def predictByXgboost(X : pd.DataFrame) -> numpy.ndarray:
 
     """Produce a prediction set with features in input and using the xgboost model """
+
+    xgboost = load('model/Model_XGboost')
 
     prediction = xgboost.predict(X)
     return prediction
@@ -28,16 +23,28 @@ def predictByXgboost(X : pd.DataFrame) -> numpy.ndarray:
 def predictByTreeClassifier(X : pd.DataFrame) -> numpy.ndarray:
     
     """Produce a prediction set with features in input and using the treeclassifier model """
+    
+    tree_classifier = load('model/Model_tree_classifier')
 
     prediction = tree_classifier.predict(X)
     return prediction
 
-def predictByGDC(X : pd.DataFrame) -> numpy.ndarray:
+def predictByGBC(X : pd.DataFrame) -> numpy.ndarray:
 
     """Produce a prediction set with features in input and using the gradient boosting classifier model """
+
+    Gradient_boosting_classifiers = load('model/gbc')
 
     prediction = Gradient_boosting_classifiers.predict(X)
     return prediction
 
 
+result = predictByGBC(X)
+
+print("result: ",result.sum())
+
+
+df = pd.DataFrame(result)
+
+df.to_csv(path_or_buf="result/prediction.csv")
 
--- README.md
+++ README.md
@@ -20,6 +20,9 @@ And if you need to remove one :
 ### poetry.lock
 If you don't want the latest version of your package or just an specific version you have to configure it here.
 
+<br>
+<br>
+
 ## Naming Convention
 ### Function
 Use a lowercase word or words. Separate words by underscores to improve readability.
@@ -50,3 +53,33 @@ Use a short, lowercase word or words. Separate words with underscores to improve
 
 Use a short, lowercase word or words. Do not separate words with underscores.
 `package`,  `mypackage`
+
+<br>
+<br>
+
+## Starting the project
+
+The first thing you must do after clonning this repository is to create a folder named "data" this will be the place where we save all the data set
+
+	from Big_data_application_folder >> mkdir data
+
+Then copy the __application_train.csv__ and the __application_test.csv__ in the data folder
+
+<br>
+
+### Now that you have your data, before predict any kind of features, you have to prepare and normalize the data
+
+	from Big_data_application_folder >> python Data-prep.py
+
+### Then execute
+
+	from Big_data_application_folder >> python Features-ingeneering.py
+
+### From this point you can choose to retrain the model or to simply use it
+
+	from Big_data_application_folder >> python Training-model.py
+
+## or
+
+	from Big_data_application_folder >> python Predict.py
+
